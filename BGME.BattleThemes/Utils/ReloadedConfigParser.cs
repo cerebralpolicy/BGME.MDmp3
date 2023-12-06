cdf1b0e45dba484bd2d4d3ea8f905bad053d4ed1
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-06 09:44:56 -0600

    Add: Simple Reloaded config parser to get Mod IDs.

---
+++ BGME.BattleThemes/Utils/ReloadedConfigParser.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace BGME.BattleThemes.Utils;
+
+internal static class ReloadedConfigParser
+{
+    public static ReloadedConfig Parse(string file)
+        => JsonSerializer.Deserialize<ReloadedConfig>(File.ReadAllText(file))!;
+}
+
+internal class ReloadedConfig
+{
+    public string ModId { get; set; } = string.Empty;
+}
