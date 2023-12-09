6e9e638a3cfad9b73a246819702df2a0d7db3c57
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-08 21:55:43 -0600

    Update battle themes service to allow for adding and removing battle themes during runtime.

--- BGME.BattleThemes/Themes/BattleThemesService.cs
+++ BGME.BattleThemes/Themes/BattleThemesService.cs
@@ -13,8 +13,12 @@ internal class BattleThemesService : IBattleThemesApi
     private readonly IBgmeApi bgme;
     private readonly MusicRegistry musicRegistry;
 
+    private readonly List<ThemePath> themePaths = new();
     private readonly List<string> themes = new();
     private readonly StringBuilder musicScriptBuilder = new();
+    private Func<string>? themeScriptCallback;
+
+    private bool hasLoaded = false;
 
     public BattleThemesService(
         IModLoader modLoader,
@@ -26,10 +30,89 @@ internal class BattleThemesService : IBattleThemesApi
         this.musicRegistry = musicRegistry;
 
         this.modLoader.ModLoading += this.OnModLoading;
-        this.modLoader.OnModLoaderInitialized += this.AddMusicThemesScript;
+        this.modLoader.OnModLoaderInitialized += this.ApplyThemeScript;
     }
 
-    public void AddFile(string modId, string filePath)
+    public void AddPath(string modId, string path)
+    {
+        this.themePaths.Add(new(modId, path));
+        if (this.hasLoaded)
+        {
+            this.ApplyThemeScript();
+        }
+
+        Log.Debug($"Added theme path.\nPath: {path}");
+    }
+
+    public void RemovePath(string path)
+    {
+        if (this.themePaths.FirstOrDefault(x => x.Path == path) is ThemePath themePath)
+        {
+            this.themePaths.Remove(themePath);
+            this.ApplyThemeScript();
+            Log.Debug($"Removed theme path.\nPath: {path}");
+        }
+        else
+        {
+            Log.Warning($"Could not find theme path to remove.\nPath: {path}");
+        }
+    }
+
+    private void OnModLoading(IModV1 mod, IModConfigV1 config)
+    {
+        if (!config.ModDependencies.Contains("BGME.BattleThemes"))
+        {
+            return;
+        }
+
+        var modDir = this.modLoader.GetDirectoryForModId(config.ModId);
+        var battleThemesDir = Path.Join(modDir, "battle-themes");
+        if (Directory.Exists(battleThemesDir))
+        {
+            this.AddPath(config.ModId, battleThemesDir);
+        }
+    }
+
+    private void ApplyThemeScript()
+    {
+        if (this.themeScriptCallback != null)
+        {
+            this.bgme.RemoveMusicScript(this.themeScriptCallback);
+            this.musicScriptBuilder.Clear();
+        }
+
+        foreach (var theme in this.themePaths)
+        {
+            if (theme.IsFile)
+            {
+                this.ProcessFile(theme.ModId, theme.Path);
+            }
+            else
+            {
+                this.ProcessFolder(theme.ModId, theme.Path);
+            }
+        }
+
+        if (this.themes.Count == 0)
+        {
+            return;
+        }
+
+        musicScriptBuilder.AppendLine($"const allThemes = [{string.Join(',', this.themes)}]");
+        musicScriptBuilder.AppendLine($"const randomizedThemes = random_music(allThemes)");
+        musicScriptBuilder.AppendLine("encounter[\"Normal Battles\"]:");
+        musicScriptBuilder.AppendLine("  music = randomizedThemes");
+        musicScriptBuilder.AppendLine("end");
+
+        var musicScript = this.musicScriptBuilder.ToString();
+        this.themeScriptCallback = () => musicScript;
+        this.bgme.AddMusicScript(this.themeScriptCallback);
+        Log.Debug($"Battle Theme Script:\n{musicScript}");
+
+        this.hasLoaded = true;
+    }
+
+    private void ProcessFile(string modId, string filePath)
     {
         try
         {
@@ -54,44 +137,16 @@ internal class BattleThemesService : IBattleThemesApi
         }
     }
 
-    public void AddFolder(string modId, string folderPath)
+    private void ProcessFolder(string modId, string folderPath)
     {
         foreach (var themeFile in Directory.EnumerateFiles(folderPath, "*.theme.pme", SearchOption.TopDirectoryOnly))
         {
-            this.AddFile(modId, themeFile);
+            this.ProcessFile(modId, themeFile);
         }
     }
 
-    private void OnModLoading(IModV1 mod, IModConfigV1 config)
+    private record ThemePath(string ModId, string Path)
     {
-        if (!config.ModDependencies.Contains("BGME.BattleThemes"))
-        {
-            return;
-        }
-
-        var modDir = this.modLoader.GetDirectoryForModId(config.ModId);
-        var battleThemesDir = Path.Join(modDir, "battle-themes");
-        if (Directory.Exists(battleThemesDir))
-        {
-            this.AddFolder(config.ModId, battleThemesDir);
-        }
-    }
-
-    private void AddMusicThemesScript()
-    {
-        if (this.themes.Count == 0)
-        {
-            return;
-        }
-
-        musicScriptBuilder.AppendLine($"const allThemes = [{string.Join(',', this.themes)}]");
-        musicScriptBuilder.AppendLine($"const randomizedThemes = random_music(allThemes)");
-        musicScriptBuilder.AppendLine("encounter[\"Normal Battles\"]:");
-        musicScriptBuilder.AppendLine("  music = randomizedThemes");
-        musicScriptBuilder.AppendLine("end");
-
-        var musicScript = this.musicScriptBuilder.ToString();
-        this.bgme.AddMusicScript(() => musicScript);
-        Log.Debug($"Battle Theme Script:\n{musicScript}");
+        public bool IsFile { get; } = File.Exists(Path);
     }
 }
